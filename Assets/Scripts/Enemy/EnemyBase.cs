fa599fb0ca474159463a58b99b69e1325330656f
lesoxtreme <38412180+lesoxtreme@users.noreply.github.com>
2025-04-21 11:23:58 -0300

    Adicionado movimentação da camera e estado de morte
     Camera acompanhando o player e vida e morte do mesmo.

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+	public int damage = 10;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log(collision.transform.name);
+
+		var health = collision.gameObject.GetComponent<HealthBase>();
+
+		if(health != null)
+		{
+			health.Damage(damage);
+		}
+	}
+
+}
