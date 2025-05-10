b5a8aa7e60b009f323e9ecc69a11c69fef441755
lesoxtreme <38412180+lesoxtreme@users.noreply.github.com>
2025-05-10 15:50:05 -0300

    Adicionado sistema de coleta de items
     Problema na colisão da moeda.

---
+++ Assets/Scripts/Items/ItemCollectableCoin.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ItemCollectableCoin : ItemCollectableBase
+{
+    protected override void OnCollect()
+    {
+        base.OnCollect();
+        ItemManager.Instance.AddCoins();
+    }
+}
