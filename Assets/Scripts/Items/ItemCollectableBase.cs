ec45302ceee18b0656f0688e3359f7c0460049e6
lesoxtreme <38412180+lesoxtreme@users.noreply.github.com>
2025-06-11 11:22:42 -0300

    Adicionado VFX nas moedas, player e cenario
     Feito varias alterações visuais.

--- Assets/Scripts/Items/ItemCollectableBase.cs
+++ Assets/Scripts/Items/ItemCollectableBase.cs
@@ -6,6 +6,14 @@ public class ItemCollectableBase : MonoBehaviour
 {
     
     public string compareTag = "Player";
+    public ParticleSystem particleSystem;
+    public float timeToHide = 3;
+    public GameObject graphicItem;
+
+    private void Awake()
+    {
+       // if(particleSystem != null) particleSystem.transform.SetParent(null);        
+    }
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,14 +23,23 @@ public class ItemCollectableBase : MonoBehaviour
         }
     }
 
+    
+
     protected virtual void Collect()
     {
-        gameObject.SetActive(false);
+        if(graphicItem !=null) graphicItem.SetActive(false);
+        Invoke("HideObject", timeToHide);
         Debug.Log("teste");
         OnCollect();
     }
+
+    private void HideObject()
+    {
+        gameObject.SetActive(false);
+    }
+
     protected virtual void OnCollect()
     {
-
+        if(particleSystem != null) particleSystem.Play();
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -17,6 +17,11 @@ public class Player : MonoBehaviour
 
 	private Animator _currentPlayer;
 
+	[Header("Jump Collision Check")]
+	public Collider2D collider2D;
+	public float distToGround;
+	public float spaceToGround = .1f;
+	public ParticleSystem jumpVFX;
 
 	private void Awake()
 	{
@@ -26,6 +31,17 @@ public class Player : MonoBehaviour
 		}
 
 		_currentPlayer = Instantiate(soPlayerSetup.player, transform);
+
+		if(collider2D != null)
+		{
+			distToGround = collider2D.bounds.extents.y;
+		}
+	}
+
+	private bool IsGrounded()
+	{
+		Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, distToGround + spaceToGround);
+		return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + spaceToGround);
 	}
 
 	private void OnPlayerKill()
@@ -37,6 +53,7 @@ public class Player : MonoBehaviour
 
    private void Update()
    {
+    IsGrounded();
 	HandleJump();
 	HandleMovement();
 	
@@ -97,14 +114,21 @@ public class Player : MonoBehaviour
 
    private void HandleJump()
    {
-	if(Input.GetKeyDown(KeyCode.Space))
+	if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
 	{
 		myRigidbody.velocity = Vector2.up * soPlayerSetup.forceJump;
 		myRigidbody.transform.localScale = Vector2.one;
 
 		DOTween.Kill(myRigidbody.transform);
 		HandleScaleJump();
+		PlayJumpVFX();
 	}
+   }
+
+   private void PlayJumpVFX()
+   {
+		VFXManager.Instance.PlayVFXByType(VFXManager.VFXType.JUMP, transform.position);
+		//if(jumpVFX != null) jumpVFX.Play();
    }
 
    private void HandleScaleJump()
